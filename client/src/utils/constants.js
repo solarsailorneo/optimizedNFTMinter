8007739b0b3a59624e62347eb9b4ae21bd35c1c8
Solar Sailor <solarsailorneo@protonmail.com>
2022-06-23 18:17:35 -0400

    Called contract on frontend and verified supply in remix

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './MyNFTMinter.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const contractAddress = '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e';
